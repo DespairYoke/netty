1b126ee2aafa96eaca09c70240f23b35980c8e95
zwd-admin <zwd-admin@zwd-admindeMac-mini.local>
2018-01-08 16:43:52 +0800

    nettyserver创建

---
+++ src/main/java/com/zapp/DiscardServer.java
@@ -0,0 +1,52 @@
+package com.zapp;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author zwd
+ * @date 2018/1/8 15:58
+ */
+public class DiscardServer {
+    private int port;
+
+    public DiscardServer(int port) {
+        this.port = port;
+    }
+    public void run() throws Exception{
+        EventLoopGroup bossGroup=new NioEventLoopGroup();
+        EventLoopGroup workerGroup=new NioEventLoopGroup();
+        try{
+            ServerBootstrap b=new ServerBootstrap();
+            b.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) {
+                            socketChannel.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    }).option(ChannelOption.SO_BACKLOG,128)
+                        .childOption(ChannelOption.SO_KEEPALIVE,true);
+            ChannelFuture f=b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new DiscardServer(port).run();
+    }
+}
---
+++ src/main/java/com/zapp/DiscardServerHandler.java
@@ -0,0 +1,35 @@
+package com.zapp;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @author zwd
+ * @date 2018/1/8 15:41
+ */
+public class DiscardServerHandler extends ChannelHandlerAdapter{
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ByteBuf in=(ByteBuf) msg; // (3)
+        try{
+            while (in.isReadable()){
+                System.out.println((char) in.readByte());
+                System.out.flush();
+            }
+        }finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
