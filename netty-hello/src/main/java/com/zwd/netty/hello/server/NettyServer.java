c81489f8ae4e18f8b3a1d1d6243264e1e4d6e084
zwd-admin <810095178@qq.com>
2018-12-24 20:38:08 +0800

    zwd

--- netty-hello/src/main/java/com/zwd/netty/hello/server/NettyServer.java
+++ netty-hello/src/main/java/com/zwd/netty/hello/server/NettyServer.java
@@ -6,8 +6,6 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-
-
 public class NettyServer {
     private static final int port = 6789; //设置服务端端口
     private static  EventLoopGroup group = new NioEventLoopGroup();   // 通过nio方式来接收连接和处理连接
